b951f38ba3d090d991e9ec4b019fa8a613073c08
ali-taj <m.a.khaleghi72@gmail.com>
2023-06-08 13:36:58 +0330

    correct user url

--- init/urls.py
+++ init/urls.py
@@ -1,14 +1,14 @@
-from api.api import DKProducts, Chat
+# from api.api import DKProducts, Chat
 from api.account import User
-from api.file import FileManager
-from api.products import Product
-from api.site_config import Main, Category
+# from api.file import FileManager
+# from api.products import Product
+# from api.site_config import Main, Category
 
 valid_urls = {
-    "chat": Chat,
-    "main": Main,
-    "category": Category,
-    "file_manager": FileManager,
+    # "chat": Chat,
+    # "main": Main,
+    # "category": Category,
+    # "file_manager": FileManager,
     # "dashboard": Dashboard,
     # "comment": Comment,
     # "bookmark": Bookmark,
@@ -18,7 +18,7 @@ valid_urls = {
     # "wallet": Wallet,
     # "affiliate": Affiliate,
     # "payment": Payment,
-    "users": User,
-    "products": Product,
-    "dk_products": DKProducts,
+    "user": User,
+    # "products": Product,
+    # "dk_products": DKProducts,
 }
